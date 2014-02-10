1b734dd6310d5890522e495be116e1ae8cdb5706
Emily Foley & <pair+EmFoley.hzla00@devbootcamp.com>
2014-02-10 11:35:02 -0800

    added view for top ten tweets, configured enviroment for use of twitter

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -1,5 +1,29 @@
-get '/' do
-  erb :index
+require 'twitter'
+
+env_config = YAML.load_file(APP_ROOT.join('config', 'twitter.yaml'))
+env_config.each do |key, value|
+  ENV[key] = value
 end
 
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['API_KEY']
+  config.consumer_secret     = ENV['API_SECRET']
+  config.access_token        = ENV['ACCESS_TOKEN']
+  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+end
+
+# p ENV
+# p ENV['API_SECRET']
+# p ENV['ACCESS_TOKEN']
+# p ENV['ACCESS_TOKEN_SECRET']
+
+get '/' do
+  "hi"
+  # erb :index
+end
+
+get '/:username' do
+  @tweets = client.user_timeline(params[:username])[0..9]
+  erb :index
+end
 
--- source/config/environment.rb
+++ source/config/environment.rb
@@ -20,6 +20,9 @@ require "sinatra/reloader" if development?
 
 require 'erb'
 
+require 'twitter'
+
+
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
@@ -41,5 +44,11 @@ end
 Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
 Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
 
+
 # Set up the database and models
 require APP_ROOT.join('config', 'database')
+
+env_config = YAML.load_file(APP_ROOT.join('config', 'twitter.yaml'))
+env_config.each do |key, value|
+  ENV[key] = value
+end
