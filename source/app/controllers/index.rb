2d8812c8ef8b1a775628fabf53c367bc60e8ab46
Eli Shkurkin <eshkurkin@gmail.com>
2014-02-10 16:27:40 -0800

    Added UI enhancements

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -9,6 +9,9 @@ end
 get '/:username' do
   @user = $client.user(params[:username])
   @username = params[:username]
+  @profile_background_image = @user.profile_background_image_url
+  @profile_image = "http://pbs.twimg.com#{@user.profile_image_uri.request_uri}"
+  p @profile_image
   erb :tweets
 end
 
