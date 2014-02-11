473638f2597aab9072981b4e915e190d273fa0b8
Aditya Mahesh & Emmanuel Kaunitz <pair+amahesh0501.mechamanny@devbootcamp.com>
2014-02-10 18:47:54 -0800

    Updated so that every 15 mins it checks for a new tweet

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -5,15 +5,25 @@ end
 get '/:username' do
 
   if User.find_by_handle(params[:username])
-    user = User.find_by_handle(params[:username])
+    @user = User.find_by_handle(params[:username])
+    if @user.tweets_stale?
+      tweets_to_delete = @user.tweets[0..9]
+      tweets_to_delete.each {|tweet| tweet.destroy}
+      tweets_from_twitter = $client.user_timeline(params[:username]).take(10)
+      tweets_from_twitter.each do |tweet|
+        Tweet.create(content: tweet.text, user_id: @user.id )
+      end
+    end
+
   else
-    user = User.create(handle: params[:username])
+    @user = User.create(handle: params[:username])
     tweets_from_twitter = $client.user_timeline(params[:username]).take(10)
     tweets_from_twitter.each do |tweet|
-    Tweet.create(content: tweet.text, user_id: user.id )
+      Tweet.create(content: tweet.text, user_id: user.id )
     end
   end
-  @tweets = user.tweets
+  @tweets = @user.tweets
+  p @tweets
   erb :tweets
 end
 
--- source/app/models/user.rb
+++ source/app/models/user.rb
@@ -1,28 +1,9 @@
 class User < ActiveRecord::Base
   has_many :tweets
 
-  # def self.find_user(args)
-  #   User.find_by_handle(args)
-  # end
+  def tweets_stale?
+    self.tweets.first.created_at < Time.now - 900
+  end
 
-  # # def self.create_user(args)
-  # #   User.create(handle: args)
-  # # end
-
-  # def self.set_user_from_twitter(user_from_db)
-  #   user_from_twitter = $client.user(user_from_db.handle)
-  # end
-
-  # def self.get_10_tweets(args)
-  #   tweets_from_twitter = $client.user_timeline(args).take(10)
-  # end
-
-  # def self.create_tweet_db(tweets_from_twitter)
-  #   tweets_from_twitter.each do |tweet|
-  #     Tweet.create(content: tweet.text, user_id: user.id )
-  #   end
-  # end
 
 end
-
-
